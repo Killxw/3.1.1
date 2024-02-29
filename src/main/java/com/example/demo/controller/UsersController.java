bf40be5092ced65a31c570330d269ee70fef74fc
Killxw <killow02@mail.ru>
2024-02-29 17:20:58 +0300

    3.1.1

--- src/main/java/com/example/demo/controller/UsersController.java
+++ src/main/java/com/example/demo/controller/UsersController.java
@@ -4,14 +4,12 @@ package com.example.demo.controller;
 import com.example.demo.model.User;
 import com.example.demo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
-@Primary
 @RequestMapping("/users")
 public class UsersController {
     private final UserService userService;
